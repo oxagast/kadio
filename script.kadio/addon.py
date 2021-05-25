e843eacaa995d7f87a0959fb52179562e37924d7
Marshall Whittaker <marshallwhittaker@gmail.com>
2021-05-24 23:51:16 -0400

    Update addon.py
    Don't know how that stuff got in here.

--- script.kadio/addon.py
+++ script.kadio/addon.py
@@ -17,19 +17,10 @@ stopcastmsg = "Shutting down radio..."
 station = xbmcgui.Dialog().input("FM Base Station Number", type=xbmcgui.INPUT_NUMERIC)
 decimal = xbmcgui.Dialog().input("FM Decimal Station", type=xbmcgui.INPUT_NUMERIC)
 startcastmsg = startcastmsg + station + "." + decimal
-<<<<<<< HEAD
 #cmd = "sudo rtl_fm -g 50 -f " + station + "." + decimal + "M -M wfm -s 180k -E deemp | sox -t raw -r 16k -e signed -b 16 -c 1  - " + audiof + " &"
 subprocess.call("sudo pkill -9 rtl_fm; sudo pkill -9 lame; sudo rm -f " + audiof, shell=True)
 cmd = "sudo rtl_fm -f " + station + "." + decimal + "M -g 50 -M fm -s 200k -A std -l 0 -E deemp -r 75k | lame -r -s 75 -V 5 --lowpass 15 --resample 44.1 -m m -b 64 - " + audiof + " &"
-#cmd = "sudo rtl_fm -f " + station + "." + decimal + "M -M wbfm | lame -r - " + audiof + " &"
-#subprocess.call('/usr/bin/watch -n 500 rm " + audiof + " &', shell=True)
-=======
-cmd = "sudo rtl_fm -f " + station + "." + decimal + "M -M wbfm | sox -t raw -r 16k -e signed -b 16 -c 1  - " + audiof + " &"
-subprocess.call('/usr/bin/watch -n 500 rm /tmp/radio.wav &', shell=True)
->>>>>>> 08319150c50bcee9b2058a1558505d771226ab24
-
 subprocess.call(cmd, shell=True)
-
 time.sleep(4)
 try:
     f = open(audiof)
